7833eda1ee4bf4497b041b0c51ec22994936c7af
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-03-23 17:35:49 -0700

    Add permutation evaluation to build script

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ jupyter nbconvert --execute integrate.ipynb --inplace --ExecutePreprocessor.time
 # Create permuted networks
 jupyter nbconvert --execute permute.ipynb --inplace --ExecutePreprocessor.timeout=-1
 
+# Evaluate permution effectiveness
+jupyter nbconvert --execute data/permuted/evaluate-permutations.ipynb --inplace --FilesWriter.build_directory=data/permuted --ExecutePreprocessor.timeout=-1
+
 # Export to neo4j
 jupyter nbconvert --execute neo4j-import.ipynb --inplace --ExecutePreprocessor.timeout=-1
 
